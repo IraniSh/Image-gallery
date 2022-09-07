0e1ca6d4b97a8214a18746e639e055daa0d623cb
Irani de Alwis <iranidealwis@gmail.com>
2022-09-08 06:44:22 +1000

    Upgrade View

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,6 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Placeholder} from '../shared/placeholder';
 
+const PLACES: Placeholder[] = [
+  {
+    id: '0',
+    name: 'Burleigh Beach',
+    image: '/assets/images/burleighBeach.png',
+    category: 'beach',
+    featured: true,
+    label: 'popular',
+    // tslint:disable-next-line:max-line-length
+    description: 'Burleigh beach is typical of the Gold Coast - superb beach, enough surf to attract local and visiting surfers.'
+  },
+  {
+    id: '1',
+    name: 'Main Beach',
+    image: '/assets/images/mainBeach.png',
+    category: 'beach',
+    featured: true,
+    label: 'popular',
+    description: 'Main Beach is a family friendly beach which is patrolled all year round by lifeguards.'
+  },
+  {
+    id: '2',
+    name: 'Surfers Paradise',
+    image: '/assets/images/surfersParadise.jpg',
+    category: 'city',
+    featured: true,
+    label: 'popular',
+    description: 'Surfers Paradise is a coastal town and suburb in the City of Gold Coast, Queensland.'
+  },
+  {
+    id: '3',
+    name: 'Kirra Beach',
+    image: '/assets/images/kirraBeach.png',
+    category: 'city',
+    featured: true,
+    label: 'popular',
+    description: 'Kirra is world famous as one of the Gold Coast\'s most iconic surfing spots.'
+  }
+]
+
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
@@ -8,52 +48,17 @@ import { Placeholder} from '../shared/placeholder';
 })
 export class GalleryComponent implements OnInit {
 
-  places: Placeholder[] = 
-  [
-    {
-      id: '0',
-      name: 'Burleigh Beach',
-      image: '/assets/images/burleighBeach.png',
-      category: 'beach',
-      featured: true,
-      label: 'popular',
-      // tslint:disable-next-line:max-line-length
-      description: 'Burleigh beach is typical of the Gold Coast - superb beach, enough surf to attract local and visiting surfers.'
-    },
-    {
-      id: '1',
-      name: 'Main Beach',
-      image: '/assets/images/mainBeach.png',
-      category: 'beach',
-      featured: true,
-      label: 'popular',
-      description: 'Main Beach is a family friendly beach which is patrolled all year round by lifeguards.'
-    },
-    {
-      id: '2',
-      name: 'Surfers Paradise',
-      image: '/assets/images/surfersParadise.jpg',
-      category: 'city',
-      featured: true,
-      label: 'popular',
-      description: 'Surfers Paradise is a coastal town and suburb in the City of Gold Coast, Queensland.'
-    },
-    {
-      id: '3',
-      name: 'Kirra Beach',
-      image: '/assets/images/kirraBeach.png',
-      category: 'city',
-      featured: true,
-      label: 'popular',
-      description: 'Kirra is world famous as one of the Gold Coast\'s most iconic surfing spots.'
-    }
-
-
-  ]
+  places = PLACES;
+  selectedPlace = PLACES[0];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(place:Placeholder)
+  {
+    this.selectedPlace = place;
   }
 
 }
